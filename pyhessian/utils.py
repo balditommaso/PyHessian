d35f2fb1e5776315a7c94cce8046327700cdef63
balditommaso <balditommaso22@gmail.com>
2026-03-26 15:32:40 +0100

    remove param without grads

--- pyhessian/utils.py
+++ pyhessian/utils.py
@@ -65,10 +65,10 @@ def get_params_grad(model):
     params = []
     grads = []
     for param in model.parameters():
-        if not param.requires_grad:
+        if not param.requires_grad or param.grad is None:
             continue
         params.append(param)
-        grads.append(torch.tensor(0.) if param.grad is None else param.grad + 0.)
+        grads.append(param.grad + 0.)
     return params, grads
 
 
